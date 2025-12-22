5324b12091e891fa003a8352a586df626397da36
K0stasCH <konstantinos.s.chlkias@gmail.com>
2025-12-22 18:21:30 +0200

    some fixes

--- src/backend/data_prep.py
+++ src/backend/data_prep.py
@@ -1,10 +1,8 @@
 from langchain_community.document_loaders import SRTLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
-from langchain_core.documents import Document
 from typing import List
-import re, os
+import re
 from .config import *
 import psycopg2
 from dotenv import load_dotenv
